90c8a2d21fca07ac53a1a7e552d9f4b09f71f6ae
Luke <294194253@qq.com>
2019-01-29 16:09:01 +0800

    回滚date分类

--- PGDatePicker/NSDate+PGCategory.h
+++ PGDatePicker/NSDate+PGCategory.h
@@ -8,5 +8,15 @@
 #import <Foundation/Foundation.h>
 
 @interface NSDate (PGCategory)
++ (NSDate *)setYear:(NSInteger)year;
++ (NSDate *)setYear:(NSInteger)year month:(NSInteger)month;
++ (NSDate *)setYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day;
++ (NSDate *)setYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day hour:(NSInteger)hour minute:(NSInteger)minute;
++ (NSDate *)setYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day hour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
++ (NSDate *)setHour:(NSInteger)hour minute:(NSInteger)minute;
++ (NSDate *)setMinute:(NSInteger)minute second:(NSInteger)second;
++ (NSDate *)setHour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
++ (NSDate *)setMonth:(NSInteger)month day:(NSInteger)day hour:(NSInteger)hour minute:(NSInteger)minute;
+- (NSInteger)howManyDaysWithMonth;
 + (NSDate *)dateFromComponents:(NSDateComponents*)dateComponents;
-@end
+    @end
